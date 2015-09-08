a7dd398965504dda780d31e9ffdeec7de2cfd8d9
arianestolfi <arianestolfi@gmail.com>
2015-09-08 02:03:46 -0300

    tentativa

--- scripts/controllers/busca.js
+++ scripts/controllers/busca.js
@@ -1,7 +1,7 @@
 app.controller('buscaController', ['$scope', '$window', '$http', '$location', function ($scope, $window, $http, $location) {
 
 
-    $http.get('http://codigorevista.org/codigo/scripts/services/items2.json').
+    $http.get('/scripts/services/items2.json').
         then(function (response) {
         // when the response is available
 
