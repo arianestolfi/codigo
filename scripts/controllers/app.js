bcc5c1afe865c5b4cf641e142badfca135885542
arianestolfi <arianestolfi@gmail.com>
2015-09-17 00:58:16 -0300

    acesso www

--- scripts/controllers/app.js
+++ scripts/controllers/app.js
@@ -23,7 +23,7 @@ app.factory("authenticationSvc", function ($http, $q, $window) {
     function login(userName, password) {
         var deferred = $q.defer();
 
-        $http.post("http://localhost/dados/service.php/auth/login", {
+        $http.post("http://www.codigorevista.org/dados/service.php/auth/login", {
             userName: administrator,
             password: dublincore
         }).then(function (result) {
